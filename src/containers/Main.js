506ea73b22cc61c848db9cbe72f06a7ae65ef680
Swarna <swarna.ilas@gmail.com>
2021-07-20 21:35:16 +0530

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,7 +4,7 @@ import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
+// import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
